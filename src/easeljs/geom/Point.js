164833583106fa7ac110ca6908be0743d244a0a1
Mike Chambers <mikechambers@gmail.com>
2011-02-16 12:06:48 -0800

    Tweaking / cleaning up docs. Converting to Yui Doc format.

--- src/easeljs/geom/Point.js
+++ src/easeljs/geom/Point.js
@@ -27,13 +27,25 @@
 * OTHER DEALINGS IN THE SOFTWARE.
 **/
 
+/**
+ * The Easel Javascript library provides a retained graphics mode for canvas 
+ * including a full, hierarchical display list, a core interaction model, and 
+ * helper classes to make working with Canvas much easier.
+ * @module EaselJS
+ */
+
 (function(window) {
 
 /**
+ * Represents a point on a 2 dimensional x / y coordinate system.
+ * @class Point
+ **/
+
+/**
 * Constructs a new Point instance.
-* @param x X position. Default is 0.
-* @param y Y position. Default is 0.
-* @class Represents a point with x / y coordinates.
+* @constructor
+* @param {Number} x X position. Default is 0.
+* @param {Number} y Y position. Default is 0.
 **/
 Point = function(x, y) {
   this.initialize(x, y);
@@ -41,13 +53,23 @@ Point = function(x, y) {
 var p = Point.prototype;
 	
 // public properties:
-	/** X position. */
+
+	/** 
+	* X position. 
+	* @property x
+	* @type {Number}
+	**/
 	p.x = 0;
-	/** Y position. */
+	
+	/** 
+	* Y position. 
+	* @property y
+	* @type {Number}
+	**/
 	p.y = 0;
 	
 // constructor:
-	/** @ignore */
+	/** @private */
 	p.initialize = function(x, y) {
 		this.x = (x == null ? 0 : x);
 		this.y = (y == null ? 0 : y);
@@ -55,7 +77,9 @@ var p = Point.prototype;
 	
 // public methods:
 	/**
-	* Returns a clone of this Point.
+	* Returns a clone of the Point instance.
+	* @method clone
+	* @return {Point} a clone of the Point instance.
 	**/
 	p.clone = function() {
 		return new Point(this.x, this.y);
@@ -63,6 +87,8 @@ var p = Point.prototype;
 
 	/**
 	* Returns a string representation of this object.
+	* @method toString
+	* @return {String} a string representation of the instance.
 	**/
 	p.toString = function() {
 		return "[Point (x="+this.x+" y="+this.y+")]";
