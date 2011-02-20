c963559ad19d96e61da7efcaf75c3bc152646294
mikechambers <mikechambers@gmail.com>
2011-02-20 10:27:25 -0800

    Tweaking docs. converting to Yui Doc format.

--- src/easeljs/display/DisplayObject.js
+++ src/easeljs/display/DisplayObject.js
@@ -27,99 +27,348 @@
 * OTHER DEALINGS IN THE SOFTWARE.
 **/
 
+/**
+* The Easel Javascript library provides a retained graphics mode for canvas 
+* including a full, hierarchical display list, a core interaction model, and 
+* helper classes to make working with Canvas much easier.
+* @module EaselJS
+**/
+
 (function(window) {
 
 /**
-* DisplayObject is an abstract class that should not be constructed directly. Instead construct subclasses such as Sprite, Bitmap, and Shape.
-* @class DisplayObject is the base class for all display classes in the CanvasDisplay library. It defines the core properties and methods that are shared between all display objects. It should not be instantiated directly.
+* DisplayObject is an abstract class that should not be constructed directly. Instead construct subclasses such as 
+* Sprite, Bitmap, and Shape. DisplayObject is the base class for all display classes in the CanvasDisplay library. 
+* It defines the core properties and methods that are shared between all display objects.
+* @class DisplayObject 
 **/
+
+
+
 DisplayObject = function() {
   this.initialize();
 }
 var p = DisplayObject.prototype;
 
-/** Suppresses errors generated when using features like hitTest, onPress/onClick, and getObjectsUnderPoint with cross domain content. */
+/**
+* Suppresses errors generated when using features like hitTest, onPress/onClick, and getObjectsUnderPoint with cross 
+* domain content
+* @property suppressCrossDomainErrors
+* @type Boolean
+* @default false
+**/
 DisplayObject.suppressCrossDomainErrors = false;
 
-/** @private */
+/**
+* @property _hitTestCanvas
+* @type HTMLCanvasElement
+* @static
+* @protected
+**/
 DisplayObject._hitTestCanvas = document.createElement("canvas");
 DisplayObject._hitTestCanvas.width = DisplayObject._hitTestCanvas.height = 1;
-/** @private */
+
+/**
+* @property _hitTestContext
+* @type CanvasRenderingContext2D
+* @static
+* @protected
+**/
 DisplayObject._hitTestContext = DisplayObject._hitTestCanvas.getContext("2d");
-/** @private */
+
+/**
+* @property _workingMatrix
+* @type Matrix2D
+* @static
+* @protected
+**/
 DisplayObject._workingMatrix = new Matrix2D();
 
-// public properties:
-	/** The alpha (transparency) for this display object. 0 is fully transparent, 1 is fully opaque. */
+
+	/** 
+	* The alpha (transparency) for this display object. 0 is fully transparent, 1 is fully opaque.
+	* @property alpha
+	* @type Number
+	* @default 1
+	**/
 	p.alpha = 1;
-	/** If a cache is active, this returns the canvas that holds the cached version of this display object. See cache() for more information. READ-ONLY. */
+	
+	/** If a cache is active, this returns the canvas that holds the cached version of this display object. See cache()
+	* for more information. READ-ONLY.
+	* @property cacheCanvas
+	* @type HTMLCanvasElement
+	* @default null
+	**/
 	p.cacheCanvas = null;
-	/** Unique ID for this display object. Makes display objects easier for some uses. */
+	
+	/**
+	* Unique ID for this display object. Makes display objects easier for some uses.
+	* @property Number
+	* @type id
+	* @default -1
+	**/
 	p.id = -1;
-	/** Indicates whether to include this object when running Stage.getObjectsUnderPoint(). Setting this to true for Sprites will cause the Sprite to be returned (not its children) regardless of whether it's mouseChildren property is true. */
+	
+	/**
+	* Indicates whether to include this object when running Stage.getObjectsUnderPoint(). Setting this to true for 
+	* Sprites will cause the Sprite to be returned (not its children) regardless of whether it's mouseChildren property 
+	* is true.
+	* @property mouseEnabled
+	* @type Boolean
+	* @default true
+	**/
 	p.mouseEnabled = true;
-	/** An optional name for this display object. Included in toString(). Useful for debugging. */
+	
+	/**
+	* An optional name for this display object. Included in toString(). Useful for debugging.
+	* @property name
+	* @type String
+	* @default null
+	**/
 	p.name = null;
-	/** A reference to the Sprite or Stage object that contains this display object, or null if it has not been added to one. READ-ONLY. */
+	
+	/**
+	* A reference to the Sprite or Stage object that contains this display object, or null if it has not been added to 
+	* one. READ-ONLY.
+	* @property parent
+	* @final
+	* @type DisplayObject
+	* @default null
+	**/
 	p.parent = null;
-	/** The x offset for this display object's registration point. For example, to make a 100x100px Bitmap rotate around it's center, you would set regX and regY to 50. */
+	
+	/**
+	* The x offset for this display object's registration point. For example, to make a 100x100px Bitmap rotate around
+	* it's center, you would set regX and regY to 50.
+	* @property regX
+	* @type Number
+	* @default 0
+	**/
 	p.regX = 0;
-	/** The y offset for this display object's registration point. For example, to make a 100x100px Bitmap rotate around it's center, you would set regX and regY to 50. */
+	
+	/**
+	* The y offset for this display object's registration point. For example, to make a 100x100px Bitmap rotate around
+	* it's center, you would set regX and regY to 50.
+	* @property regX
+	* @type Number
+	* @default 0
+	**/
 	p.regY = 0;
-	/** The rotation in degrees for this display object. */
+	
+	/**
+	* The rotation in degrees for this display object.
+	* @property rotation
+	* @type Number
+	* @default 0
+	**/
 	p.rotation = 0;
-	/** The factor to stretch this display object horizontally. For example, setting scaleX to 2 will stretch the display object to twice it's nominal width. */
+	
+	/**
+	* The factor to stretch this display object horizontally. For example, setting scaleX to 2 will stretch the display
+	* object to twice it's nominal width.
+	* @property scaleX
+	* @type Number
+	* @default 1
+	**/
 	p.scaleX = 1;
-	/** The factor to stretch this display object vertically. For example, setting scaleY to 0.5 will stretch the display object to half it's nominal height. */
+	
+	/**
+	* The factor to stretch this display object vertically. For example, setting scaleY to 0.5 will stretch the display
+	* object to half it's nominal height.
+	* @property scaleY
+	* @type Number
+	* @default 1
+	**/
 	p.scaleY = 1;
-	/** The factor to skew this display object horizontally. */
+	
+	/**
+	* The factor to skew this display object horizontally.
+	* @property skewX
+	* @type Number
+	* @default 0
+	**/
 	p.skewX = 0;
-	/** The factor to skew this display object vertically. */
+	
+	/**
+	* The factor to skew this display object vertically.
+	* @property skewY
+	* @type Number
+	* @default 0
+	**/
 	p.skewY = 0;
-	/** A shadow object that defines the shadow to render on this display object. Set to null to remove a shadow. If null, this property is inherited from the parent container. */
+	
+	/**
+	* A shadow object that defines the shadow to render on this display object. Set to null to remove a shadow. If 
+	* null, this property is inherited from the parent container.
+	* @property shadow
+	* @type Shadow
+	* @default null
+	**/
 	p.shadow = null;
-	/** Indicates whether this display object should be rendered to the canvas and included when running Stage.getObjectsUnderPoint(). */
+	
+	/**
+	* Indicates whether this display object should be rendered to the canvas and included when running
+	* Stage.getObjectsUnderPoint().
+	* @property visible
+	* @type Boolean
+	* @default true
+	**/
 	p.visible = true;
-	/** The x (horizontal) position of the display object, relative to its parent. */
+	
+	/**
+	* The x (horizontal) position of the display object, relative to its parent.
+	* @property x
+	* @type Number
+	* @default 0
+	**/
 	p.x = 0;
-	/** The y (vertical) position of the display object, relative to its parent. */
+	
+	/** The y (vertical) position of the display object, relative to its parent.
+	* @property y
+	* @type Number
+	* @default 0
+	**/
 	p.y = 0;
-	/** The composite operation indicates how the pixels of this display object will be composited with the elements behind it. If null, this property is inherited from the parent container. For more information, read the <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#compositing">whatwg spec on compositing</a>. */
+	
+	/**
+	* The composite operation indicates how the pixels of this display object will be composited with the elements 
+	* behind it. If null, this property is inherited from the parent container. For more information, read the 
+	* <a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#compositing">
+	* whatwg spec on compositing</a>.
+	* @property compositeOperation
+	* @type String
+	* @default null
+	**/
 	p.compositeOperation = null;
-	/** Indicates whether the display object should have it's x & y position rounded prior to drawing it to stage. This only applies if the enclosing stage has snapPixelsEnabled set to true, and the display object's composite transform does not include any scaling, rotation, or skewing. The snapToPixel property is true by default for Bitmap and BitmapSequence instances, and false for all other display objects. */
+	
+	/**
+	* Indicates whether the display object should have it's x & y position rounded prior to drawing it to stage. 
+	* This only applies if the enclosing stage has snapPixelsEnabled set to true, and the display object's composite 
+	* transform does not include any scaling, rotation, or skewing. The snapToPixel property is true by default for 
+	* Bitmap and BitmapSequence instances, and false for all other display objects.
+	* @property snapToPixel
+	* @type Boolean
+	* @default false
+	**/
 	p.snapToPixel = false;
-	/** The onPress callback is called when the user presses down on their mouse over this display object. The handler is passed a single param containing the corresponding MouseEvent instance. You can subscribe to the onMouseMove and onMouseUp callbacks of the event object to receive these events until the user releases the mouse button. If an onPress handler is set on a container, it will receive the event if any of its children are clicked. */
+	
+	/**
+	* The onPress callback is called when the user presses down on their mouse over this display object. The handler 
+	* is passed a single param containing the corresponding MouseEvent instance. You can subscribe to the onMouseMove
+	* and onMouseUp callbacks of the event object to receive these events until the user releases the mouse button. 
+	* If an onPress handler is set on a container, it will receive the event if any of its children are clicked.
+	* @event onPress
+	* @param {MouseEvent} event MouseEvent with information about the event.
+	**/
 	p.onPress = null;
-	/** The onClick callback is called when the user presses down on and then releases the mouse button over this display object. The handler is passed a single param containing the corresponding MouseEvent instance. If an onClick handler is set on a container, it will receive the event if any of its children are clicked. */
+	
+	/**
+	* The onClick callback is called when the user presses down on and then releases the mouse button over this 
+	* display object. The handler is passed a single param containing the corresponding MouseEvent instance. If an 
+	* onClick handler is set on a container, it will receive the event if any of its children are clicked.
+	* @event onClick
+	* @param {MouseEvent} event MouseEvent with information about the event.
+	**/
 	p.onClick = null;
-	/** The onMouseOver callback is called when the user rolls over the display object. You must enable this event using stage.enableMouseOver(). The handler is passed a single param containing the corresponding MouseEvent instance.  */
+	
+	/**
+	* The onMouseOver callback is called when the user rolls over the display object. You must enable this event using 
+	* stage.enableMouseOver(). The handler is passed a single param containing the corresponding MouseEvent instance.
+	* @event onMouseOver
+	* @param {MouseEvent} event MouseEvent with information about the event.
+	**/
 	p.onMouseOver = null;
-	/** The onMouseOut callback is called when the user rolls off of the display object. You must enable this event using stage.enableMouseOver(). The handler is passed a single param containing the corresponding MouseEvent instance.  */
+	
+	/**
+	* The onMouseOut callback is called when the user rolls off of the display object. You must enable this event using
+	* stage.enableMouseOver(). The handler is passed a single param containing the corresponding MouseEvent instance.
+	* @event onMouseOut
+	* @param {MouseEvent} event MouseEvent with information about the event.
+	**/
 	p.onMouseOut = null;
 	
 // private properties:
-	/** @private */
+
+	/**
+	* @property _cacheOffsetX
+	* @protected
+	* @type Number
+	* @default 0
+	**/
 	p._cacheOffsetX = 0;
-	/** @private */
+	
+	/**
+	* @property _cacheOffsetY
+	* @protected
+	* @type Number
+	* @default 0
+	**/
 	p._cacheOffsetY = 0;
-	/** @private */
+	
+	/**
+	* @property _cacheDraw
+	* @protected
+	* @type Boolean
+	* @default false
+	**/
 	p._cacheDraw = false;
-	/** @private */
+	
+	/**
+	* @property _activeContext
+	* @protected
+	* @type CanvasRenderingContext2D
+	* @default null
+	**/
 	p._activeContext = null;
-	/** @private */
+	
+	/**
+	* @property _restoreContext
+	* @protected
+	* @type Boolean
+	* @default false
+	**/
 	p._restoreContext = false;
-	/** @private */
+	
+	/**
+	* @property _revertShadow
+	* @protected
+	* @type Boolean
+	* @default false
+	**/
 	p._revertShadow = false;
-	/** @private */
+	
+	/**
+	* @property _revertX
+	* @protected
+	* @type Number
+	* @default 0
+	**/
 	p._revertX = 0;
-	/** @private */
+	
+	/**
+	* @property _revertY
+	* @protected
+	* @type Number
+	* @default 0
+	**/
 	p._revertY = 0;
-	/** @private */
+	
+	/**
+	* @property
+	* @protected
+	* @type
+	* @default
+	**/
 	p._revertAlpha = 1;
 	
 // constructor:
 	// separated so it can be easily addressed in subclasses:
-	/** @ignore */
+
+	/** 
+	* Initialization method.
+	* @method initialize
+	* @protected
+	*/
 	p.initialize = function() {
 		this.id = UID.get();
 		this.children = [];
@@ -156,11 +405,19 @@ DisplayObject._workingMatrix = new Matrix2D();
 	}
 	
 	/**
-	* Draws the display object into a new canvas, which is then used for subsequent draws. For complex content that does not change frequently (ex. a Sprite with many children that do not move, or a complex vector Shape), this can provide for much faster rendering because the content does not need to be re-rendered each tick. The cached display object can be moved, rotated, faded, etc freely, however if it's content changes, you must manually update the cache by calling updateCache() or cache() again. You must specify the cache area via the x, y, w, and h parameters. This defines the rectangle that will be rendered and cached using this display object's coordinates. For example if you defined a Shape that drew a circle at 0, 0 with a radius of 25, you could call myShape.cache(-25, -25, 50, 50) to cache the full shape.
-	* @param x
-	* @param y
-	* @param width
-	* @param height
+	* Draws the display object into a new canvas, which is then used for subsequent draws. For complex content 
+	* that does not change frequently (ex. a Sprite with many children that do not move, or a complex vector Shape),
+	* this can provide for much faster rendering because the content does not need to be re-rendered each tick. The 
+	* cached display object can be moved, rotated, faded, etc freely, however if it's content changes, you must manually
+	* update the cache by calling updateCache() or cache() again. You must specify the cache area via the x, y, w, 
+	* and h parameters. This defines the rectangle that will be rendered and cached using this display object's 
+	* coordinates. For example if you defined a Shape that drew a circle at 0, 0 with a radius of 25, you could call 
+	* myShape.cache(-25, -25, 50, 50) to cache the full shape.
+	* @method cache
+	* @param {Number} x The x coordinate origin for the cache region.
+	* @param {Number} y The y coordinate origin for the cache region.
+	* @param {Number} width The width of the cache region.
+	* @param {Number} height The height of the cache region.
 	**/
 	p.cache = function(x, y, width, height) {
 		// draw to canvas.
@@ -170,7 +427,8 @@ DisplayObject._workingMatrix = new Matrix2D();
 		this.cacheCanvas.width = width;
 		this.cacheCanvas.height = height;
 		ctx.setTransform(1, 0, 0, 1, -x, -y);
-		ctx.clearRect(0, 0, width+1, height+1); // because some browsers don't correctly clear if the width/height remain the same.
+		ctx.clearRect(0, 0, width+1, height+1); // because some browsers don't correctly clear if the width/height 
+												//remain the same.
 		this.draw(ctx, true);
 		this._cacheOffsetX = x;
 		this._cacheOffsetY = y;
