d5a65a969ad7645a7f683c213349197cd1c2ea47
Kevin Moore <kevin@thinkpixellab.com>
2011-02-25 01:29:36 -0800

    Clean-up to Point, extend goog.math.Coordinate

--- src/easeljs/geom/Point.js
+++ src/easeljs/geom/Point.js
@@ -4,7 +4,7 @@
 *
 *
 * Copyright (c) 2010 Grant Skinner
-* 
+*
 * Permission is hereby granted, free of charge, to any person
 * obtaining a copy of this software and associated documentation
 * files (the "Software"), to deal in the Software without
@@ -13,10 +13,10 @@
 * copies of the Software, and to permit persons to whom the
 * Software is furnished to do so, subject to the following
 * conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be
 * included in all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,65 +28,41 @@
 */
 
 /**
-* The Easel Javascript library provides a retained graphics mode for canvas 
-* including a full, hierarchical display list, a core interaction model, and 
-* helper classes to make working with Canvas much easier.
-**/
+ * The Easel Javascript library provides a retained graphics mode for canvas
+ * including a full, hierarchical display list, a core interaction model, and
+ * helper classes to make working with Canvas much easier.
+ **/
+
+goog.require('goog.math.Coordinate');
 
 goog.provide('Point');
 
 /**
-* Represents a point on a 2 dimensional x / y coordinate system.
-* @class Point
-* @constructor
-* @param {number} x X position. Default is 0.
-* @param {number} y Y position. Default is 0.
-**/
+ * Represents a point on a 2 dimensional x / y coordinate system.
+ * @class Point
+ * @constructor
+ * @extends {goog.math.Coordinate}
+ * @param {number=} x X position. Default is 0.
+ * @param {number=} y Y position. Default is 0.
+ **/
 Point = function(x, y) {
-  this.initialize(x, y);
-}
-var p = Point.prototype;
-	
-// public properties:
+  goog.math.Coordinate.call(this, x, y);
+};
+goog.inherits(Point, goog.math.Coordinate);
 
-	/** 
-	* X position. 
-	* @property x
-	* @type {number}
-	**/
-	p.x = 0;
-	
-	/** 
-	* Y position. 
-	* @property y
-	* @type {number}
-	**/
-	p.y = 0;
-	
-// constructor:
-	/** 
-	* Initialization method.
-	* @protected
-	*/
-	p.initialize = function(x, y) {
-		this.x = (x == null ? 0 : x);
-		this.y = (y == null ? 0 : y);
-	}
-	
 // public methods:
-	/**
-	* Returns a clone of the Point instance.
-	* @return {Point} a clone of the Point instance.
-	**/
-	p.clone = function() {
-		return new Point(this.x, this.y);
-	}
+/**
+ * Returns a clone of the Point instance.
+ * @return {Point} a clone of the Point instance.
+ **/
+Point.prototype.clone = function() {
+  return new Point(this.x, this.y);
+};
 
-	/**
-	* Returns a string representation of this object.
-	* @return {string} a string representation of the instance.
-	**/
-	p.toString = function() {
-		return "[Point (x="+this.x+" y="+this.y+")]";
-	}
-	
+/**
+ * Returns a string representation of this object.
+ * @return {string} a string representation of the instance.
+ **/
+Point.prototype.toString = function() {
+  return "[Point (x=" + this.x + " y=" + this.y + ")]";
+};
