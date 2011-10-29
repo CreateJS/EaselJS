e58608d5e530ecaf44d2d4c8c2f94ba0b1163493
Alek Storm <alek.storm@gmail.com>
2011-10-29 01:33:27 -0400

    Split input files to Closure Compiler call onto multiple lines

--- build/build.bash
+++ build/build.bash
@@ -18,7 +18,25 @@ if [ -e $FINAL ];then
 	rm $FINAL
 fi
 
-java -jar $1 --js ../src/easeljs/utils/UID.js --js ../src/easeljs/utils/Ticker.js --js ../src/easeljs/events/MouseEvent.js --js ../src/easeljs/geom/Matrix2D.js --js ../src/easeljs/geom/Point.js --js ../src/easeljs/geom/Rectangle.js --js ../src/easeljs/display/Shadow.js --js ../src/easeljs/display/SpriteSheet.js --js ../src/easeljs/display/Graphics.js --js ../src/easeljs/display/DisplayObject.js --js ../src/easeljs/display/Container.js --js ../src/easeljs/display/Stage.js --js ../src/easeljs/display/Bitmap.js --js ../src/easeljs/display/BitmapSequence.js --js ../src/easeljs/display/Shape.js --js ../src/easeljs/display/Text.js --js ../src/easeljs/utils/SpriteSheetUtils.js --js_output_file $OUTPUT
+java -jar $1 \
+    --js_output_file $OUTPUT \
+    --js ../src/easeljs/utils/UID.js \
+    --js ../src/easeljs/utils/Ticker.js \
+    --js ../src/easeljs/events/MouseEvent.js \
+    --js ../src/easeljs/geom/Matrix2D.js \
+    --js ../src/easeljs/geom/Point.js \
+    --js ../src/easeljs/geom/Rectangle.js \
+    --js ../src/easeljs/display/Shadow.js \
+    --js ../src/easeljs/display/SpriteSheet.js \
+    --js ../src/easeljs/display/Graphics.js \
+    --js ../src/easeljs/display/DisplayObject.js \
+    --js ../src/easeljs/display/Container.js \
+    --js ../src/easeljs/display/Stage.js \
+    --js ../src/easeljs/display/Bitmap.js \
+    --js ../src/easeljs/display/BitmapSequence.js \
+    --js ../src/easeljs/display/Shape.js \
+    --js ../src/easeljs/display/Text.js \
+    --js ../src/easeljs/utils/SpriteSheetUtils.js
 
 cat license.txt >> $FINAL
 cat $OUTPUT >> $FINAL
