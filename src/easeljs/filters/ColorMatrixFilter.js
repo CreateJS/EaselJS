94a8bb8e772e48e3cfb443732c97f6226490f65a
Mike Chambers <mikechambers@gmail.com>
2011-11-24 10:31:00 -0800

    fixed issue where there was an = instead of == in a conditional check (making the statement unreachable).

--- src/easeljs/filters/ColorMatrixFilter.js
+++ src/easeljs/filters/ColorMatrixFilter.js
@@ -78,8 +78,8 @@ var p = ColorMatrixFilter.prototype = new Filter();
 	**/
 	p.applyFilter = function(ctx, x, y, width, height, targetCtx, targetX, targetY) {
 		targetCtx = targetCtx || ctx;
-		if (targetX = null) { targetX = x; }
-		if (targetY = null) { targetY = y; }
+		if (targetX == null) { targetX = x; }
+		if (targetY == null) { targetY = y; }
 		try {
 			var imageData = ctx.getImageData(x, y, width, height);
 		} catch(e) {
