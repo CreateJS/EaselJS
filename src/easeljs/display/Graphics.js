f27286549ce051428d55777be6a87190965ad915
Grant Skinner <info@gskinner.com>
2012-03-07 20:39:24 -0700

    Added Graphics.decodePath, and added support for negative corner radii on round rects.
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- src/easeljs/display/Graphics.js
+++ src/easeljs/display/Graphics.js
@@ -87,6 +87,7 @@ var p = Graphics.prototype;
 
 // static public methods:
 	
+	
 	/**
 	 * Returns a CSS compatible color string based on the specified RGB numeric color values in the format 
 	 * "rgba(255,255,255,1.0)", or if alpha is null then in the format "rgb(255,255,255)". For example,
@@ -137,6 +138,16 @@ var p = Graphics.prototype;
 			return "hsla("+(hue%360)+","+saturation+"%,"+lightness+"%,"+alpha+")";
 		}
 	}
+	
+	/**
+	 * Map of Base64 characters to values. Used by decodePath().
+	 * @property BASE_64
+	 * @static
+	 * @final
+	 * @type Object
+	 **/
+	Graphics.BASE_64 = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25,"a":26,"b":27,"c":28,"d":29,"e":30,"f":31,"g":32,"h":33,"i":34,"j":35,"k":36,"l":37,"m":38,"n":39,"o":40,"p":41,"q":42,"r":43,"s":44,"t":45,"u":46,"v":47,"w":48,"x":49,"y":50,"z":51,"0":52,"1":53,"2":54,"3":55,"4":56,"5":57,"6":58,"7":59,"8":60,"9":61,"+":62,"/":63};
+		
 	
 	/**
 	 * Maps numeric values for the caps parameter of setStrokeStyle to corresponding string values.
@@ -519,7 +530,7 @@ var p = Graphics.prototype;
 	 * @return {Graphics} The Graphics instance the method is called on (useful for chaining calls.)
 	 **/
 	p.endFill = function() {
-		this.beginFill(null);
+		this.beginFill();
 		return this;
 	}
 	
@@ -621,7 +632,7 @@ var p = Graphics.prototype;
 	 * @return {Graphics} The Graphics instance the method is called on (useful for chaining calls.)
 	 **/
 	p.endStroke = function() {
-		this.beginStroke(null);
+		this.beginStroke();
 		return this;
 	}
 	
@@ -669,16 +680,25 @@ var p = Graphics.prototype;
 	 **/
 	p.drawRoundRectComplex = function(x, y, w, h, radiusTL, radiusTR, radiusBR, radiusBL) {
 		this._dirty = this._active = true;
+		var pi = Math.PI, arc=this._ctx.arc, lineTo=this._ctx.lineTo;
 		this._activeInstructions.push(
 			new Command(this._ctx.moveTo, [x+radiusTL, y]),
-			new Command(this._ctx.lineTo, [x+w-radiusTR, y]),
-			new Command(this._ctx.arc, [x+w-radiusTR, y+radiusTR, radiusTR, (-Math.PI/2), 0, false]),
-			new Command(this._ctx.lineTo, [x+w, y+h-radiusBR]),
-			new Command(this._ctx.arc, [x+w-radiusBR, y+h-radiusBR, radiusBR, 0, Math.PI/2, false]),
-			new Command(this._ctx.lineTo, [x+radiusBL, y+h]),
-			new Command(this._ctx.arc, [x+radiusBL, y+h-radiusBL, radiusBL, Math.PI/2, Math.PI, false]),
-			new Command(this._ctx.lineTo, [x, y+radiusTL]),
-			new Command(this._ctx.arc, [x+radiusTL, y+radiusTL, radiusTL, Math.PI, Math.PI*3/2, false])
+			new Command(lineTo, [x+w-radiusTR, y]),
+			(radiusTR>=0) ?
+				new Command(arc, [x+w-radiusTR, y+radiusTR, radiusTR, -pi/2, 0]) :
+				new Command(arc, [x+w, y, -radiusTR, pi, pi/2, true]) ,
+			new Command(lineTo, [x+w, y+h-radiusBR]),
+			(radiusBL>=0) ?
+				new Command(arc, [x+w-radiusBR, y+h-radiusBR, radiusBR, 0, pi/2]) :
+				new Command(arc, [x+w, y+h, -radiusBR, -pi/2, pi, true]) ,
+			new Command(lineTo, [x+radiusBL, y+h]),
+			(radiusBL>=0) ?
+				new Command(arc, [x+radiusBL, y+h-radiusBL, radiusBL, pi/2, pi]) :
+				new Command(arc, [x, y+h, -radiusBL, 0, -pi/2, true]) ,
+			new Command(lineTo, [x, y+radiusTL]),
+			(radiusTL>=0) ?
+				new Command(arc, [x+radiusTL, y+radiusTL, radiusTL, pi, -pi/2]) :
+				new Command(arc, [x, y, -radiusTL, pi/2, 0, true])
 		);
 		return this;
 	} 
@@ -769,6 +789,69 @@ var p = Graphics.prototype;
 			}
 			angle += a;
 			this._activeInstructions.push(new Command(this._ctx.lineTo, [x+Math.cos(angle)*radius, y+Math.sin(angle)*radius]));
+		}
+		return this;
+	}
+	
+	/**
+	 * Decodes a compact encoded path string into a series of draw instructions.
+	 * This format is not intended to be human readable, and is meant for use by authoring tools.
+	 * The format uses a base64 character set, with each character representing 6 bits, to define a series of draw commands.
+	 * 
+	 * Each command is comprised of a single "header" character followed by a variable number of alternating x and y position values.
+	 * Reading the header bits from left to right (most to least significant): bits 1 to 3 specify the type of operation
+	 * (0-moveTo, 1-lineTo, 2-quadraticCurveTo, 3-bezierCurveTo, 4-7 unused). Bit 4 indicates whether position values use 12 bits (2 characters) 
+	 * or 18 bits (3 characters), with a one indicating the latter. Bits 5 and 6 are currently unused.
+	 *
+	 * Following the header is a series of 2 (moveTo, lineTo), 4 (quadraticCurveTo), or 6 (bezierCurveTo) parameters.
+	 * These parameters are alternating x/y positions represented by 2 or 3 characters (as indicated by the 4th bit in the command char).
+	 * These characters consist of a 1 bit sign (1 is negative, 0 is positive), followed by an 11 (2 char) or 17 (3 char) bit integer value.
+	 * All position values are in tenths of a pixel.
+	 * Except in the case of move operations, this value is a delta from the previous x or y position (as appropriate).
+	 * 
+	 * For example, the string "A3cAAMAu4AAA" represents a line starting at -150,0 and ending at 150,0.
+	 * A - bits 000000. First 3 bits (000) indicate a moveTo operation. 4th bit (0) indicates 2 chars per parameter.
+	 * n0 - 110111011100. Absolute x position of -150.0px. First bit indicates a negative value, remaining bits indicate 1500 tenths of a pixel. 
+	 * AA - 000000000000. Absolute y position of 0.
+	 * I - 001100. First 3 bits (001) indicate a lineTo operation. 4th bit (1) indicates 3 chars per parameter.
+	 * Au4 - 000000101110111000. An x delta of 300.0px, which is added to the previous x value of -150.0px to provide an absolute position of +150.0px.
+	 * AAA - 000000000000000000. A y delta value of 0.
+	 *
+	 * @method decodePath
+	 * @param {String} str The path string to decode.
+	 * @return {Graphics} A clone of the current Graphics instance.
+	 **/
+	p.p = p.decodePath = function(str) {
+		var instructions = [this.moveTo, this.lineTo, this.quadraticCurveTo, this.bezierCurveTo];
+		var paramCount = [2, 2, 4, 6];
+		var i=0, l=str.length;
+		var params = [];
+		var x=0, y=0;
+		var base64 = Graphics.BASE_64;
+		
+		while (i<l) {
+			var n = base64[str.charAt(i)];
+			var fi = n>>3; // highest order bits 1-3 code for operation.
+			var f = instructions[fi];
+			// check that we have a valid instruction & that the unused bits are empty:
+			if (!f || (n&3)) { throw("bad path data"); return this; }
+			var pl = paramCount[fi];
+			if (!fi) { x=y=0; }
+			params.length = 0;
+			i++;
+			var charCount = (n>>2&1)+2;  // 4th header bit indicates number size for this operation.
+			for (var p=0; p<pl; p++) {
+				var num = base64[str.charAt(i)];
+				var sign = (num>>5) ? -1 : 1;
+				num = ((num&31)<<6)|(base64[str.charAt(i+1)]);
+				if (charCount == 3) { num = (num<<6)|(base64[str.charAt(i+2)]); }
+				num = sign*num/10;
+				if (p%2) { x = (num += x); }
+				else { y = (num += y); }
+				params[p] = num;
+				i += charCount;
+			}
+			f.apply(this,params);
 		}
 		return this;
 	}
@@ -983,6 +1066,13 @@ var p = Graphics.prototype;
 	 * type Function
 	 **/
 	p.dp = p.drawPolyStar;
+	
+	/** Shortcut to decodePath.
+	 * @property p
+	 * @protected
+	 * type Function
+	 **/
+	p.p
 	
 	
 // private methods:
