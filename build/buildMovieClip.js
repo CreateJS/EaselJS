9bdbc89a7721c2ef3aa9d408d03420c331fd1916
Grant Skinner <info@gskinner.com>
2012-04-13 21:55:03 -0600

    Moved tools dir to root to fix GitHub language detection.
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- build/buildMovieClip.js
+++ build/buildMovieClip.js
@@ -35,8 +35,8 @@ var TEMPLATE_DIR_PATH = "template";
 var TMP_DIR_NAME = "tmp";
 
 // paths to tools:
-var GOOGLE_CLOSURE_PATH = "tools/google-closure/compiler.jar";
-var YUI_DOC_PATH = "tools/yuidoc/bin/yuidoc.py";
+var GOOGLE_CLOSURE_PATH = "../tools/google-closure/compiler.jar";
+var YUI_DOC_PATH = "../tools/yuidoc/bin/yuidoc.py";
 
 // yui version being used
 var YUI_VERSION = 2;
