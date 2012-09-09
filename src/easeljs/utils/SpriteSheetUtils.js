44a77aee2390effd4bfb7741f940ff4e6c85665e
Grant Skinner <info@gskinner.com>
2012-09-09 16:59:35 +0100

    Fixes to SpriteSheetUtils documentation (added .addFlippedFrames, removed .flip)
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- src/easeljs/utils/SpriteSheetUtils.js
+++ src/easeljs/utils/SpriteSheetUtils.js
@@ -62,20 +62,24 @@ var SpriteSheetUtils = function() {
 
 // public static methods:
 	/**
-	 * <b>This is an experimental method, and is likely to be buggy. Please report issues.</b><br/><br/>
-	 * Extends the existing sprite sheet by flipping the original frames either horizontally, vertically, or both,
-	 * and adding appropriate animation & frame data.
-	 * @method flip
+	 * <b>This is an experimental method, and may be buggy. Please report issues.</b><br/><br/>
+	 * Extends the existing sprite sheet by flipping the original frames horizontally, vertically, or both,
+	 * and adding appropriate animation & frame data. The flipped animations will have a suffix added to their names
+	 * (_h, _v, _hv as appropriate).
+	 * <br/><br/>
+	 * For example:<br/>
+	 * SpriteSheetUtils.addFlippedFrames(mySpriteSheet, true, true);
+	 * The above would add frames that are flipped horizontally AND frames that are flipped vertically.
+	 * <br/><br/>
+	 * Note that you can also flip any display object by setting its scaleX or scaleY to a negative value. On some
+	 * browsers (especially those without hardware accelerated canvas) this can result in slightly degraded performance,
+	 * which is why addFlippedFrames is available. 
+	 * @method addFlippedFrames
 	 * @static
-	 * @param {Image} spriteSheet The sprite sheet to use as the source.
-	 * @param {Object} flipData A generic object that specifies which frames will be flipped, what to name the
-	 * flipped result, and how to flip the frames (horizontally, vertically, or both). Each property name
-	 * indicates the name of a new sequence to create, and should reference an array where the first index is
-	 * the name of the original sequence to flip, the second index indicates whether to flip it horizontally,
-	 * the third index indicates whether to flip it vertically, and the fourth indicates what the "next" value
-	 * for the resulting frame data should be. For example, the following would create a new sequence named
-	 * "walk_left" consisting of the frames from the original "walk_right" sequence flipped
-	 * horizontally: &#123;walk_left: ["walk_right", true, false]&#125;
+	 * @param {SpriteSheet} spriteSheet 
+	 * @param {Boolean} horizontal If true, horizontally flipped frames will be added.
+	 * @param {Boolean} vertical If true, vertically flipped frames will be added.
+	 * @param {Boolean} both If true, frames that are flipped both horizontally and vertically will be added.
 	 **/
 	SpriteSheetUtils.addFlippedFrames = function(spriteSheet, horizontal, vertical, both) {
 		if (!horizontal && !vertical && !both) { return; }
