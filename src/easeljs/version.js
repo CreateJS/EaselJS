b1f09fd9f20aa5b9f8a31a668145778027711a07
Grant Skinner <info@gskinner.com>
2013-01-07 14:51:23 -0700

    Added version.js.
    Signed-off-by: Grant Skinner <info@gskinner.com>

---
+++ src/easeljs/version.js
@@ -0,0 +1,27 @@
+(function() {
+
+/**
+ * Static class holding library specific information such as the version and buildDate of
+ * the library.
+ * @class EaselJS
+ **/
+var o = this.createjs = this.createjs||{};
+o = (o.EaselJS = o.EaselJS||{});
+ 
+/**
+ * The version string for this release.
+ * @property version
+ * @type String
+ * @static
+ **/
+o.version = /*version*/"NEXT"; // injected by build process
+
+/**
+ * The build date for this release in UTC format.
+ * @property buildDate
+ * @type String
+ * @static
+ **/
+o.buildDate = /*date*/"Mon, 07 Jan 2013 21:47:57 GMT"; // injected by build process
+
+})();
