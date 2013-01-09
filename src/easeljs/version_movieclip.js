1b702ba01d4a3bf749288e9350c79a2f0c454432
Lanny McNie <lanny@gskinner.com>
2013-01-09 15:59:17 -0700

    Added version file for movie clip.
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

---
+++ src/easeljs/version_movieclip.js
@@ -0,0 +1,27 @@
+(function() {
+
+/**
+ * Static class holding library specific information such as the version and buildDate of
+ * the library.
+ * @class MovieClip
+ **/
+var o = this.createjs = this.createjs||{};
+o = (o.MovieClip = o.MovieClip||{});
+ 
+/**
+ * The version string for this release.
+ * @property version
+ * @type String
+ * @static
+ **/
+o.version = /*version*/"NEXT"; // injected by build process
+
+/**
+ * The build date for this release in UTC format.
+ * @property buildDate
+ * @type String
+ * @static
+ **/
+o.buildDate = /*date*/"Wed, 09 Jan 2013 04:14:26 GMT"; // injected by build process
+
+})();
