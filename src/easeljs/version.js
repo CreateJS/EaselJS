d32e04bc2f220e038f6e9d36a832846e43c4b49a
Grant Skinner <info@gskinner.com>
2013-02-04 20:16:26 -0700

    Added Stage.handleEvent().
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- src/easeljs/version.js
+++ src/easeljs/version.js
@@ -22,6 +22,6 @@ o.version = /*version*/"NEXT"; // injected by build process
  * @type String
  * @static
  **/
-o.buildDate = /*date*/"Mon, 04 Feb 2013 02:45:08 GMT"; // injected by build process
+o.buildDate = /*date*/"Tue, 05 Feb 2013 02:29:17 GMT"; // injected by build process
 
 })();
