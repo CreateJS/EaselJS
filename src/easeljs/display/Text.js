9c12cebb61fa67b18f2ee1a8cbba26d0a1da4ae4
aaharu <aaharu@hotmail.com>
2013-05-18 02:22:08 +0900

    fix documentation

--- src/easeljs/display/Text.js
+++ src/easeljs/display/Text.js
@@ -333,7 +333,7 @@ var p = Text.prototype = new createjs.DisplayObject();
 	/** 
 	 * @method _drawTextLine
 	 * @param {CanvasRenderingContext2D} ctx
-	 * @param {Text} text
+	 * @param {String} text
 	 * @param {Number} y
 	 * @protected 
 	 **/
