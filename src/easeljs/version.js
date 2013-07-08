7c2c140e23e448e2fbdd50b1cff3dc5180969509
Grant Skinner <info@gskinner.com>
2013-07-08 13:17:57 -0600

    Updated the lib & docs.
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- src/easeljs/version.js
+++ src/easeljs/version.js
@@ -22,6 +22,6 @@ o.version = /*version*/"NEXT"; // injected by build process
  * @type String
  * @static
  **/
-o.buildDate = /*date*/"Tue, 11 Jun 2013 16:42:52 GMT"; // injected by build process
+o.buildDate = /*date*/"Mon, 08 Jul 2013 19:15:27 GMT"; // injected by build process
 
 })();
