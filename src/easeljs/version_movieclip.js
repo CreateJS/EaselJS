928a99ea01df1985c38df1c27807584633353f35
Grant Skinner <info@gskinner.com>
2013-10-10 10:57:17 -0600

    fixed an issue related to modal dialogs and pointer position for mouse down
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- src/easeljs/version_movieclip.js
+++ src/easeljs/version_movieclip.js
@@ -25,6 +25,6 @@ this.createjs = this.createjs || {};
 	 * @type String
 	 * @static
 	 **/
-	s.buildDate = /*date*/"Wed, 09 Oct 2013 14:52:50 GMT"; // injected by build process
+	s.buildDate = /*date*/"Thu, 10 Oct 2013 16:56:30 GMT"; // injected by build process
 
 })();
