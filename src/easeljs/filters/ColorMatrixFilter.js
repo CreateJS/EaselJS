cad1f3b3f77a50c3e5670cf2eea4984ff288bc7e
Michael Kuenzli <michaelkuenzli@gmail.com>
2014-08-27 15:51:27 +0200

    Move performance-intensive calculation of applyFilter in separate method to avoid bail-out of V8-compiler due to try/catch-block.

--- src/easeljs/filters/ColorMatrixFilter.js
+++ src/easeljs/filters/ColorMatrixFilter.js
@@ -91,25 +91,7 @@ var p = ColorMatrixFilter.prototype = new createjs.Filter();
 			//if (!this.suppressCrossDomainErrors) throw new Error("unable to access local image data: " + e);
 			return false;
 		}
-		var data = imageData.data;
-		var l = data.length;
-		var r,g,b,a;
-		var mtx = this.matrix;
-		var m0 =  mtx[0],  m1 =  mtx[1],  m2 =  mtx[2],  m3 =  mtx[3],  m4 =  mtx[4];
-		var m5 =  mtx[5],  m6 =  mtx[6],  m7 =  mtx[7],  m8 =  mtx[8],  m9 =  mtx[9];
-		var m10 = mtx[10], m11 = mtx[11], m12 = mtx[12], m13 = mtx[13], m14 = mtx[14];
-		var m15 = mtx[15], m16 = mtx[16], m17 = mtx[17], m18 = mtx[18], m19 = mtx[19];
-
-		for (var i=0; i<l; i+=4) {
-			r = data[i];
-			g = data[i+1];
-			b = data[i+2];
-			a = data[i+3];
-			data[i] = r*m0+g*m1+b*m2+a*m3+m4; // red
-			data[i+1] = r*m5+g*m6+b*m7+a*m8+m9; // green
-			data[i+2] = r*m10+g*m11+b*m12+a*m13+m14; // blue
-			data[i+3] = r*m15+g*m16+b*m17+a*m18+m19; // alpha
-		}
+		p._applyFilter(imageData.data, this.matrix);
 		targetCtx.putImageData(imageData, targetX, targetY);
 		return true;
 	};
@@ -125,6 +107,34 @@ var p = ColorMatrixFilter.prototype = new createjs.Filter();
 	 **/
 	p.clone = function() {
 		return new ColorMatrixFilter(this.matrix);
+	};
+
+// private methods:
+	/**
+	 * @method _applyFilter
+	 * @param {Object} data
+	 * @param {Array} matrix
+	 * @protected
+	**/
+	p._applyFilter = function(data, matrix) {
+		var l = data.length;
+		var r,g,b,a;
+		var mtx = matrix;
+		var m0 =  mtx[0],  m1 =  mtx[1],  m2 =  mtx[2],  m3 =  mtx[3],  m4 =  mtx[4];
+		var m5 =  mtx[5],  m6 =  mtx[6],  m7 =  mtx[7],  m8 =  mtx[8],  m9 =  mtx[9];
+		var m10 = mtx[10], m11 = mtx[11], m12 = mtx[12], m13 = mtx[13], m14 = mtx[14];
+		var m15 = mtx[15], m16 = mtx[16], m17 = mtx[17], m18 = mtx[18], m19 = mtx[19];
+
+		for (var i=0; i<l; i+=4) {
+			r = data[i];
+			g = data[i+1];
+			b = data[i+2];
+			a = data[i+3];
+			data[i] = r*m0+g*m1+b*m2+a*m3+m4; // red
+			data[i+1] = r*m5+g*m6+b*m7+a*m8+m9; // green
+			data[i+2] = r*m10+g*m11+b*m12+a*m13+m14; // blue
+			data[i+3] = r*m15+g*m16+b*m17+a*m18+m19; // alpha
+		}
 	};
 
 	createjs.ColorMatrixFilter = ColorMatrixFilter;
