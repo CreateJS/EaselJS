c36a6e6ffdf4e8d795d0fb4c347aab36cd1ad033
Grant Skinner <info@gskinner.com>
2014-10-28 23:18:35 -0600

    Update inheritance tutorial.
    Signed-off-by: Grant Skinner <info@gskinner.com>

--- tutorials/Inheritance/Button.js
+++ tutorials/Inheritance/Button.js
@@ -1,50 +1,49 @@
 (function() {
 
-var Button = function(label, color) {
-  this.initialize(label, color);
-}
-var p = Button.prototype = new createjs.Container(); // inherit from Container
-Button.prototype.constructor = Button;
-
-p.label;
-p.background;
-p.count = 0;
-
-p.Container_initialize = p.initialize;
-p.initialize = function(label, color) {
-	this.Container_initialize();
+function Button(label, color) {
+	this.Container_constructor();
 	
+	this.color = color;
 	this.label = label;
-	if (!color) { color = "#CCC"; }
 	
-	var text = new createjs.Text(label, "20px Arial", "#000");
+	this.setup();
+}
+var p = createjs.extend(Button, createjs.Container);
+
+
+p.setup = function() {
+	var text = new createjs.Text(this.label, "20px Arial", "#000");
 	text.textBaseline = "top";
 	text.textAlign = "center";
 	
 	var width = text.getMeasuredWidth()+30;
 	var height = text.getMeasuredHeight()+20;
 	
-	this.background = new createjs.Shape();
-	this.background.graphics.beginFill(color).drawRoundRect(0,0,width,height,10);
-	
 	text.x = width/2;
 	text.y = 10;
 	
-	this.addChild(this.background,text); 
+	var background = new createjs.Shape();
+	background.graphics.beginFill(this.color).drawRoundRect(0,0,width,height,10);
+	
+	this.addChild(background, text); 
 	this.on("click", this.handleClick);
-	this.on("tick", this.handleTick);
+	this.on("rollover", this.handleRollOver);
+	this.on("rollout", this.handleRollOver);
+	this.cursor = "pointer";
 
 	this.mouseChildren = false;
-} 
+	
+	this.offset = Math.random()*10;
+	this.count = 0;
+} ;
 
-p.handleClick = function (event) {    
-	var target = event.target;
-	alert("You clicked on a button: "+ target.label);
-} 
+p.handleClick = function (event) {
+	alert("You clicked on a button: "+this.label);
+} ;
 
-p.handleTick = function(event) {       
-	p.alpha = Math.cos(p.count++*0.1)*0.4+0.6;
-}
+p.handleRollOver = function(event) {       
+	this.alpha = event.type == "rollover" ? 0.4 : 1;
+};
 
-window.Button = Button;
+window.Button = createjs.promote(Button);
 }());
