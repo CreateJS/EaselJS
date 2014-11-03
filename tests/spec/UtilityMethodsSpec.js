d385e04a522b94d197736c5d7ea1f8308bf1a932
Wes Gorgichuk <wes@gskinner.com>
2014-11-03 16:43:19 -0700

    Added SpriteSheetBuilder spec

--- tests/spec/UtilityMethodsSpec.js
+++ tests/spec/UtilityMethodsSpec.js
@@ -58,4 +58,51 @@ describe("Utlity Methods", function () {
 		});
 
 	});
+
+	it("SpriteSheetBuilder", function(done) {
+		var circle = new createjs.Shape();
+		circle.graphics.beginFill("#F00").drawCircle(0,0,30).beginFill("#C00").drawCircle(0,0,10);
+		circle.setTransform(0,0);
+
+		var square = new createjs.Container();
+		var squareBg = square.addChild(new createjs.Shape());
+		squareBg.graphics.beginFill("#00F").drawRect(0,0,80,80);
+
+		var squareFld = square.addChild(new createjs.Text("1", "bold 72px Arial", "#9BF"));
+		squareFld.textBaseline = "top";
+		squareFld.textAlign = "center";
+		squareFld.x = 40;
+		square.bounds = new createjs.Rectangle(0,0,80,80);
+
+		// create the sprite sheet builder:
+		var builder = new createjs.SpriteSheetBuilder();
+
+		var index = builder.addFrame(circle, new createjs.Rectangle(-30, -30,60,60));
+		builder.addAnimation("circle", index);
+
+		var frames = [];
+		for (var i=0; i<5; i++) {
+			index = builder.addFrame(square, null, 1, function(target, data) { squareFld.text=data; }, i);
+			frames.push(index);
+		}
+
+		builder.addAnimation("square", [1], true);
+		builder.addAnimation("square2", [2], true);
+
+		var spriteSheet = builder.build();
+
+		var circle2 = new createjs.Sprite(spriteSheet, "circle");
+		this.stage.addChild(circle2).set({x:40, y:120});
+
+		var circle3 = new createjs.Sprite(spriteSheet, "circle");
+		this.stage.addChild(circle3).set({x:120, y:120});
+
+		var square2 = new createjs.Sprite(spriteSheet, "square");
+		this.stage.addChild(square2);
+
+		var square3 = new createjs.Sprite(spriteSheet, "square2");
+		this.stage.addChild(square3).set({x:80});
+
+		this.compareBaseLine("assets/SpriteSheetBuilder.png", done, expect);
+	});
 });
