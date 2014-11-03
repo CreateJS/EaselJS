72a2ac37058ba035129acb303dff848fd30cf7bd
Wes Gorgichuk <wes@gskinner.com>
2014-11-03 15:40:23 -0700

    Added tests for Text and BitmapText, some formatting updates are included.

--- tests/spec/DisplayListSpec.js
+++ tests/spec/DisplayListSpec.js
@@ -21,24 +21,24 @@ describe("DisplayList", function () {
 
 		// Used to test clone operations
 		displayObjectProps = {
-			alpha:.5,
-			name:"foo",
-			regX:5,
-			regY:5,
-			rotation:90,
-			scaleX:1.5,
-			scaleY:1.6,
-			shadow:shadow,
-			skewX:5,
-			skewY:5,
+			alpha: .5,
+			name: "foo",
+			regX: 5,
+			regY: 5,
+			rotation: 90,
+			scaleX: 1.5,
+			scaleY: 1.6,
+			shadow: shadow,
+			skewX: 5,
+			skewY: 5,
 			visible: false,
-			x:15,
-			y:15,
-			mouseEnabled:true,
-			compositeOperation:"copy"
+			x: 15,
+			y: 15,
+			mouseEnabled: true,
+			compositeOperation: "copy"
 		};
 
-		bitmapProps = {sourceRect: new createjs.Rectangle(1,2,3,4)};
+		bitmapProps = {sourceRect: new createjs.Rectangle(1, 2, 3, 4)};
 		for (var n in displayObjectProps) {
 			bitmapProps[n] = displayObjectProps[n];
 		}
@@ -109,7 +109,7 @@ describe("DisplayList", function () {
 		expect(stage.getStage()).toBe(stage);
 	});
 
-	it("Bitmap.clone();", function() {
+	it("Bitmap.clone();", function () {
 		var bmp = new createjs.Bitmap(image).set(bitmapProps);
 		var clone = bmp.clone();
 
@@ -127,7 +127,7 @@ describe("DisplayList", function () {
 		}
 	});
 
-	it("DisplayObject.clone();", function(){
+	it("DisplayObject.clone();", function () {
 		var obj = new createjs.DisplayObject().set(displayObjectProps);
 		var clone = obj.clone();
 
@@ -273,9 +273,9 @@ describe("DisplayList", function () {
 
 		var container = new createjs.Container();
 
-		var foo = new createjs.Shape().set({y:80});
-		var bar = new createjs.Shape().set({y:45});
-		var ja = new createjs.Shape().set({y:150});
+		var foo = new createjs.Shape().set({y: 80});
+		var bar = new createjs.Shape().set({y: 45});
+		var ja = new createjs.Shape().set({y: 150});
 
 		container.addChild(ja, bar, foo);
 		expect(container.getChildIndex(ja)).toBe(0);
@@ -283,7 +283,7 @@ describe("DisplayList", function () {
 		expect(container.getChildIndex(ja)).toBe(2);
 	});
 
-	it("swapChildren() should work.", function() {
+	it("swapChildren() should work.", function () {
 		var container = new createjs.Container();
 
 		var foo = new createjs.Shape();
@@ -299,7 +299,7 @@ describe("DisplayList", function () {
 		expect(container.getChildIndex(bar)).toBe(1);
 	});
 
-	it("swapChildrenAt() should work.", function() {
+	it("swapChildrenAt() should work.", function () {
 		var container = new createjs.Container();
 
 		var foo = new createjs.Shape();
@@ -316,11 +316,11 @@ describe("DisplayList", function () {
 		expect(container.getChildIndex(bars)).toBe(0);
 	});
 
-	it("toDataURL() should return a valid image.", function(done) {
+	it("toDataURL() should return a valid image.", function (done) {
 		var url = stage.toDataURL("#ffffff");
 
 		var image = new Image();
-		image.onload = function() {
+		image.onload = function () {
 			done();
 		}
 
@@ -328,12 +328,12 @@ describe("DisplayList", function () {
 		expect(image.src).not.toBe(null);
 	});
 
-	it("stage.toString() should work.", function() {
+	it("stage.toString() should work.", function () {
 		expect(stage.toString()).not.toBe(null);
 	});
 
-	it("cache(), updateCache() and uncache() should work.", function() {
-		stage.cache(0,0,25,25);
+	it("cache(), updateCache() and uncache() should work.", function () {
+		stage.cache(0, 0, 25, 25);
 		expect(stage.cacheCanvas).not.toBe(null);
 		stage.updateCache();
 		stage.uncache();
@@ -345,5 +345,105 @@ describe("DisplayList", function () {
 			err = true;
 		}
 		expect(err).toBe(true);
+	});
+
+	it("Text", function (done) {
+		var txt = new createjs.Text("", "12px Arial", "#dd0000");
+		txt.text = "This text is rendered in canvas.\n\nusing the Text Object.!\n\nTEST!\n\nmore text";
+		txt.textBaseline = "top";
+		txt.textAlign = "left";
+		txt.y = 0;
+		txt.x = 0;
+		this.stage.addChild(txt);
+
+		this.compareBaseLine("assets/Text.png", done, expect, 0.01);
+	});
+
+	it("BitmapText", function (done) {
+		var _this = this;
+
+		var img = new Image();
+		img.onload = function () {
+			var ss = new createjs.SpriteSheet(data);
+			var text = new createjs.BitmapText("abcdef\nghijklm\nnopqr\nstuvw\nxyz!,.?", ss);
+			_this.stage.addChild(text);
+
+			// Need to delay this for Safari.
+			setTimeout(function () {
+				_this.stage.update();
+				_this.compareBaseLine("assets/BitmapText.png", done, expect);
+			}, 5);
+		};
+
+		img.src = "assets/BitmapFontSpriteSheet.png";
+
+		// Embedded SpriteSheet data.
+		var data = {
+			"animations": {
+				"V": {"frames": [21]},
+				"A": {"frames": [0]},
+				",": {"frames": [26]},
+				"W": {"frames": [22]},
+				"B": {"frames": [1]},
+				"X": {"frames": [23]},
+				"C": {"frames": [2]},
+				".": {"frames": [29]},
+				"Y": {"frames": [24]},
+				"D": {"frames": [3]},
+				"Z": {"frames": [25]},
+				"E": {"frames": [4]},
+				"F": {"frames": [5]},
+				"G": {"frames": [6]},
+				"H": {"frames": [7]},
+				"I": {"frames": [8]},
+				"J": {"frames": [9]},
+				"K": {"frames": [10]},
+				"!": {"frames": [27]},
+				"L": {"frames": [11]},
+				"M": {"frames": [12]},
+				"N": {"frames": [13]},
+				"O": {"frames": [14]},
+				"P": {"frames": [15]},
+				"Q": {"frames": [16]},
+				"R": {"frames": [17]},
+				"S": {"frames": [18]},
+				"T": {"frames": [19]},
+				"?": {"frames": [28]},
+				"U": {"frames": [20]}
+			},
+			"images": ["assets/BitmapFontSpriteSheet.png"],
+			"frames": [
+				[155, 2, 25, 41, 0, -10, -3],
+				[72, 2, 28, 43, 0, -8, -1],
+				[599, 2, 28, 38, 0, -8, -4],
+				[41, 2, 27, 44, 0, -8, -1],
+				[728, 2, 32, 38, 0, -6, -4],
+				[184, 2, 35, 41, 0, -4, -2],
+				[409, 2, 30, 39, 0, -7, -3],
+				[443, 2, 29, 39, 0, -7, -3],
+				[901, 2, 13, 35, 0, -8, -5],
+				[698, 2, 26, 38, 0, -9, -4],
+				[666, 2, 28, 38, 0, -8, -4],
+				[764, 2, 23, 38, 0, -10, -4],
+				[828, 2, 37, 36, 0, -3, -5],
+				[567, 2, 28, 38, 0, -8, -4],
+				[519, 2, 44, 38, 0, 1, -4],
+				[869, 2, 28, 36, 0, -8, -5],
+				[476, 2, 39, 38, 0, -2, -4],
+				[371, 2, 34, 39, 0, -5, -3],
+				[631, 2, 31, 38, 0, -6, -4],
+				[289, 2, 39, 40, 0, -2, -3],
+				[918, 2, 31, 32, 0, -6, -7],
+				[791, 2, 33, 37, 0, -5, -4],
+				[2, 2, 35, 46, 0, -4, 1],
+				[253, 2, 32, 40, 0, -6, -3],
+				[104, 2, 32, 43, 0, -6, -1],
+				[332, 2, 35, 39, 0, -5, -4],
+				[953, 2, 9, 16, 0, -17, -29],
+				[140, 2, 11, 41, 0, -16, -1],
+				[223, 2, 26, 41, 0, -7, -1],
+				[966, 2, 9, 10, 0, -17, -31]
+			]
+		};
 	});
 });
