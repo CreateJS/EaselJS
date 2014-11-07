7bfe6ba5d7da4522b811b0753fd5d8ff281de7bf
Wes Gorgichuk <wes@gskinner.com>
2014-11-07 09:50:43 -0700

    During a build, remove all license headers, and only attach one at the top. Also insert comments with each classes filename, to make the code more legible. Fixed version not being set during a full build.

--- build/tasks/updateversion.js
+++ build/tasks/updateversion.js
@@ -4,8 +4,13 @@ module.exports = function (grunt) {
 		var newValues = {date:new Date().toUTCString(), version:this.data.version};
 		replaceMetaData(this.data.file, newValues);
 	});
-	
+
 	grunt.registerMultiTask('clearversion', function() {
+		// Don't clear the version when building everything (the combined build will run this after its done)
+		var buildArgs = grunt.config("buildArgs");
+		if (buildArgs && buildArgs[0] == "all") {
+			return;
+		}
 		replaceMetaData(this.data.file, {date:"", version:""});
 	});
 
@@ -14,9 +19,9 @@ module.exports = function (grunt) {
 			grunt.log.error(file+' not found.');
 			return;
 		}
-		
+
 		var str = grunt.file.read(file);
-	
+
 	    for(var n in values) {
 	        var pattern = new RegExp("(\/\\*="+n+"\\*\/\")(.*)(\";)", "g");
 	        var result = pattern.test(str);
