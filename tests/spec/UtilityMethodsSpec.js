d31f60d37e91091695ddd27a78f2e3162c8b8697
Wes  Gorgichuk <wes@gskinner.com>
2014-12-01 10:59:28 -0700

    Added more robust unit tests for ticker.

--- tests/spec/UtilityMethodsSpec.js
+++ tests/spec/UtilityMethodsSpec.js
@@ -2,6 +2,7 @@ describe("Utlity Methods", function () {
 
 	beforeEach(function () {
 		createjs.Ticker.reset();
+		createjs.Ticker.init();
 	});
 
 	it("indexOf()", function () {
@@ -9,52 +10,106 @@ describe("Utlity Methods", function () {
 		expect(createjs.indexOf(arr, 3)).toBe(2);
 	});
 
-	describe("Ticker", function (done) {
+	describe("Ticker", function () {
+		it("createjs.Ticker.addEventListener(tick) evt.time", function (done) {
+			setTimeout(function () {
+				var tick = function (evt) {
+					expect(evt.time).toBeInRange(createjs.Ticker.getTime(), 2);
+					done();
 
-		it("createjs.Ticker.addEventListener(tick)", function (done) {
-			var tick = function () {
-				expect(true).toBe(true);
-				done();
-
-				createjs.Ticker.removeEventListener("tick", tick);
-			}
-			createjs.Ticker.addEventListener("tick", tick);
+					createjs.Ticker.removeEventListener("tick", tick);
+				}
+				createjs.Ticker.addEventListener("tick", tick);
+			}, 1000);
 		});
 
-		it("setPaused", function () {
+		it("createjs.Ticker.addEventListener(tick) evt.delta", function (done) {
+			setTimeout(function () {
+				var tick = function (evt) {
+					expect(evt.delta).toBeInRange(createjs.Ticker.getInterval(), 5);
+					done();
+
+					createjs.Ticker.removeEventListener("tick", tick);
+				}
+				createjs.Ticker.addEventListener("tick", tick);
+			}, 1000);
+		});
+
+		it("createjs.Ticker.addEventListener(tick) evt.runTime", function (done) {
+			setTimeout(function () {
+				var tick = function (evt) {
+					expect(evt.runTime).toBeInRange(createjs.Ticker.getTime() | 0, 1);
+					done();
+
+					createjs.Ticker.removeEventListener("tick", tick);
+				}
+				createjs.Ticker.addEventListener("tick", tick);
+			}, 1000);
+		});
+
+		it("setPaused", function (done) {
 			createjs.Ticker.setPaused(true);
+
+			// Should be paused
 			expect(createjs.Ticker.getPaused()).toBe(true);
+
+			// tick event should also also be paused.
+			var func = function (evt) {
+				expect(evt.paused).toBe(true);
+				createjs.Ticker.removeEventListener("tick", func);
+				done();
+			};
+			createjs.Ticker.addEventListener("tick", func);
 		});
 
 		it("getFPS", function () {
 			createjs.Ticker.setInterval(40);
+
 			expect(createjs.Ticker.getFPS()).toBe(25);
 			expect(createjs.Ticker.getInterval()).toBe(40);
 		});
 
-		it("setFPS", function () {
+		it("setFPS", function (done) {
 			createjs.Ticker.setFPS(40);
-			expect(createjs.Ticker.getFPS()).toBe(40);
+			setTimeout(function () {
+				expect(createjs.Ticker.getTime() | 0).toBeInRange(40 * 2, 2);
+				done();
+			}, 40 * 2);
 		});
 
-		it("getMeasuredFPS", function () {
-			expect(createjs.Ticker.getMeasuredFPS()).not.toBe(null);
+		it("getMeasuredFPS", function (done) {
+			setTimeout(function () {
+				expect(createjs.Ticker.getMeasuredFPS()).toBeInRange(createjs.Ticker.getFPS(), 5);
+				done();
+			}, 1000);
 		});
 
 		it("getMeasuredTickTime", function () {
-			expect(createjs.Ticker.getMeasuredTickTime()).not.toBe(null);
+			expect(createjs.Ticker.getMeasuredTickTime() | 0).not.toBe(null);
 		});
 
-		it("getTicks", function () {
-			expect(createjs.Ticker.getTicks()).not.toBe(null);
+		it("getTicks", function (done) {
+			setTimeout(function () {
+				expect(createjs.Ticker.getTicks()).toBeInRange(createjs.Ticker.getFPS(), 5);
+				done();
+			}, 1000);
 		});
 
-		it("getTime", function () {
-			expect(createjs.Ticker.getTime()).not.toBe(null);
+		it("should advance by 1 seconds - getTime()", function (done) {
+			setTimeout(function () {
+				expect(createjs.Ticker.getTime()).toBeInRange(1000, 5);
+				done();
+			}, 1000);
 		});
 
-		it("getEventTime", function () {
-			expect(createjs.Ticker.getEventTime()).not.toBe(null);
+		it("getEventTime", function (done) {
+			setTimeout(function () {
+				var expected = (createjs.Ticker.getInterval() * createjs.Ticker.getFPS()) - createjs.Ticker.getInterval();
+
+				// Firefox needs the higest range (on average)
+				expect(createjs.Ticker.getEventTime() | 0).toBeInRange(expected, 40);
+				done();
+			}, 1000);
 		});
 	});
 
