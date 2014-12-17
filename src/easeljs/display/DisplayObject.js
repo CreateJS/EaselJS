597021fb7fa1f02be8cf93f2a7cdc8d27590208c
Yaroslav Sivakov <yar3333@gmail.com>
2014-12-17 19:36:14 +0300

    Update DisplayObject.js
    setBound(null) bugfix

--- src/easeljs/display/DisplayObject.js
+++ src/easeljs/display/DisplayObject.js
@@ -1161,8 +1161,7 @@ this.createjs = this.createjs||{};
 	 * @param {Number} height The height of the bounds.
 	 **/
 	p.setBounds = function(x, y, width, height) {
-		if (x == null) { this._bounds = x; }
-		this._bounds = (this._bounds || new createjs.Rectangle()).setValues(x, y, width, height);
+		this._bounds = x != null ? (this._bounds || new createjs.Rectangle()).setValues(x, y, width, height) : null;
 	};
 
 	/**
