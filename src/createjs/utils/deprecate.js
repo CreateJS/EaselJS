a85ee08f5a17e70ad604ca08713d81414b13e1b7
Lanny McNie <lannymcnie@users.noreply.github.com>
2017-06-30 14:20:19 -0600

    Updated deprecate utility to be iOS-safe.

--- src/createjs/utils/deprecate.js
+++ src/createjs/utils/deprecate.js
@@ -63,7 +63,8 @@ this.createjs = this.createjs||{};
 createjs.deprecate = function(fallbackMethod, name) {
 	"use strict";
 	return function() {
-		console && (console.warn || console.log)("Deprecated property or method '"+name+"'. See docs for info.");
+		var msg = "Deprecated property or method '"+name+"'. See docs for info.";
+		console && (console.warn ? console.warn(msg) : console.log(msg));
 		return fallbackMethod && fallbackMethod.apply(this, arguments);
 	}
 };
