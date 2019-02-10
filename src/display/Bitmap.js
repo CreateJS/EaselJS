7617734747785d91028759c30ba9aa508268e20e
Patrick McGuckin <p.mcgooks@gmail.com>
2019-02-10 16:03:26 -0700

    Update Bitmap v2.0 with latest 1.0 updates

--- src/display/Bitmap.js
+++ src/display/Bitmap.js
@@ -1,8 +1,8 @@
 /**
- * @license Bitmap
+ * Bitmap
  * Visit http://createjs.com/ for documentation, updates and examples.
  *
- * Copyright (c) 2017 gskinner.com, inc.
+ * Copyright (c) 2010 gskinner.com, inc.
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -24,6 +24,8 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @license
  */
 
 import DisplayObject from "./DisplayObject";
@@ -35,39 +37,38 @@ import VideoBuffer from "../utils/VideoBuffer";
  *
  * <strong>Notes:</strong>
  * <ol>
- *     <li>When using a video source that may loop or seek, use a {@link easeljs.VideoBuffer} object to
- *      blinking / flashing.
- *     <li>When a string path or image tag that is not yet loaded is used, the stage may need to be redrawn before it
- *      will be displayed.</li>
- *     <li>Bitmaps with an SVG source currently will not respect an alpha value other than 0 or 1. To get around this,
- *     the Bitmap can be cached.</li>
- *     <li>Bitmaps with an SVG source will taint the canvas with cross-origin data, which prevents interactivity. This
- *     happens in all browsers except recent Firefox builds.</li>
- *     <li>Images loaded cross-origin will throw cross-origin security errors when interacted with using a mouse, using
- *     methods such as `getObjectUnderPoint`, or using filters, or caching. You can get around this by setting
- *     `crossOrigin` flags on your images before passing them to EaselJS, eg: `img.crossOrigin="Anonymous";`</li>
+ * 	<li>When using a video source that may loop or seek, use a {@link easeljs.VideoBuffer} object to
+ * 	 prevent blinking / flashing.
+ * 	<li>When a string path or image tag that is not yet loaded is used, the stage may need to be redrawn before it
+ * 	 will be displayed.</li>
+ * 	<li>Bitmaps with an SVG source currently will not respect an alpha value other than 0 or 1. To get around this,
+ * 	the Bitmap can be cached.</li>
+ * 	<li>Bitmaps with an SVG source will taint the canvas with cross-origin data, which prevents interactivity. This
+ * 	happens in all browsers except recent Firefox builds.</li>
+ * 	<li>Images loaded cross-origin will throw cross-origin security errors when interacted with using a mouse, using
+ * 	methods such as `getObjectUnderPoint`, or using filters, or caching. You can get around this by setting
+ * 	`crossOrigin` flags on your images before passing them to EaselJS, eg: `img.crossOrigin="Anonymous";`</li>
  * </ol>
  *
  * @memberof easeljs
  * @extends easeljs.DisplayObject
  * @example
- * const bitmap = new Bitmap("imagePath.jpg");
- *
- * @param {CanvasImageSource | String | Object} imageOrUri The source image to display. This can be a CanvasImageSource
- * (image, video, canvas), an object with a `getImage` method that returns a CanvasImageSource, or a string URL to an image.
- * If the latter, a new Image instance with the URL as its src will be used.
+ * const bitmap = new Bitmap("./imagePath.jpg");
  */
 export default class Bitmap extends DisplayObject {
 
-	constructor (imageOrUri) {
-		super();
-
+	/**
+	 * @param {CanvasImageSource|String|Object} imageOrUri The source image to display. This can be a CanvasImageSource
+	 * (image, video, canvas), an object with a `getImage` method that returns a CanvasImageSource, or a string URL to an image.
+	 * If the latter, a new Image instance with the URL as its src will be used.
+	 */
+	constructor(imageOrUri) {
 		/**
 		 * The source image to display. This can be a CanvasImageSource
 		 * (image, video, canvas), an object with a `getImage` method that returns a CanvasImageSource, or a string URL to an image.
 		 * If the latter, a new Image instance with the URL as its src will be used.
 		 * @property image
-		 * @type {CanvasImageSource | Object}
+		 * @type {CanvasImageSource|Object}
 		 */
 		if (typeof imageOrUri === "string") {
 			this.image = document.createElement("img");
@@ -78,7 +79,11 @@ export default class Bitmap extends DisplayObject {
 
 		/**
 		 * Specifies an area of the source image to draw. If omitted, the whole image will be drawn.
-		 * Note that video sources must have a width / height set to work correctly with `sourceRect`.
+		 * Notes:
+		 * <ul>
+		 *     <li>that video sources must have a width / height set to work correctly with `sourceRect`</li>
+		 *     <li>Cached objects will ignore the `sourceRect` property</li>
+		 * </ul>
 		 * @type {easeljs.Rectangle}
 		 * @default null
 		 */
@@ -92,20 +97,21 @@ export default class Bitmap extends DisplayObject {
 		this._webGLRenderStyle = DisplayObject._StageGL_BITMAP;
 	}
 
-	isVisible () {
-		let image = this.image;
-		let hasContent = this.cacheCanvas || (image && (image.naturalWidth || image.getContext || image.readyState >= 2));
+	isVisible() {
+		const image = this.image;
+		const hasContent = this.cacheCanvas || (image && (image.naturalWidth || image.getContext || image.readyState >= 2));
 		return !!(this.visible && this.alpha > 0 && this.scaleX != 0 && this.scaleY != 0 && hasContent);
 	}
 
-	draw (ctx, ignoreCache = false) {
+	draw(ctx, ignoreCache = false) {
 		if (super.draw(ctx, ignoreCache)) { return true; }
-		let img = this.image, rect = this.sourceRect;
-		if (img instanceof VideoBuffer) { img = img.getImage(); }
-		if (img == null) { return true; }
+		const rect = this.sourceRect;
+		let img = this.image;
+		if (img.getImage) { img = img.getImage(); }
+		if (!img) { return true; }
 		if (rect) {
 			// some browsers choke on out of bound values, so we'll fix them:
-			let x1 = rect.x, y1 = rect.y, x2 = x1 + rect.width, y2 = y1 + rect.height, x = 0, y = 0, w = img.width, h = img.height;
+			const x1 = rect.x, y1 = rect.y, x2 = x1 + rect.width, y2 = y1 + rect.height, x = 0, y = 0, w = img.width, h = img.height;
 			if (x1 < 0) { x -= x1; x1 = 0; }
 			if (x2 > w) { x2 = w; }
 			if (y1 < 0) { y -= y1; y1 = 0; }
@@ -115,6 +121,40 @@ export default class Bitmap extends DisplayObject {
 			ctx.drawImage(img, 0, 0);
 		}
 		return true;
+	}
+
+	getBounds() {
+		const rect = super.getBounds();
+		if (rect) { return rect; }
+		const image = this.image;
+		const o = this.sourceRect || image;
+		const hasContent = (image && (image.naturalWidth || image.getContext || image.readyState >= 2));
+		return hasContent ? this._rectangle.setValues(0, 0, o.width, o.height) : null;
+	}
+
+	/**
+	 * Returns a clone of the Bitmap instance.
+	 * @param {Boolean} [node=false] Whether the underlying dom element should be cloned as well.
+	 * @return {Bitmap} a clone of the Bitmap instance.
+	 */
+	clone(node = false) {
+		const image = this.image;
+		if (image && node) {
+			image = image.cloneNode();
+			image.src = image.src; // IE cloneNode bug fix
+		}
+		const o = new Bitmap(image);
+		if (this.sourceRect) { o.sourceRect = this.sourceRect.clone(); }
+		this._cloneProps(o);
+		return o;
+	}
+
+	/**
+	 * Returns a string representation of this object.
+	 * @return {String} a string representation of the instance.
+	 */
+	toString() {
+		return `[Bitmap (name=${this.name})]`;
 	}
 
 	// Note, the doc sections below document using the specified APIs (from DisplayObject) from
@@ -149,27 +189,5 @@ export default class Bitmap extends DisplayObject {
 	 *
 	 * @alias easeljs.Bitmap#uncache
 	 */
-
-	getBounds () {
-		let rect = super.getBounds();
-		if (rect) { return rect; }
-		let image = this.image, o = this.sourceRect || image;
-		let hasContent = (image && (image.naturalWidth || image.getContext || image.readyState >= 2));
-		return hasContent ? this._rectangle.setValues(0, 0, o.width, o.height) : null;
-	}
-
-	/**
-	 * Returns a clone of the Bitmap instance.
-	 * @param {Boolean} [node] Whether the underlying DOM element should be cloned as well.
-	 * @return {easeljs.Bitmap} A clone of the Bitmap instance.
-	 */
-	clone (node) {
-		let img = this.image;
-		if (img != null && node != null) { img = img.cloneNode(); }
-		let bmp = new Bitmap(img);
-		if (this.sourceRect) { bmp.sourceRect = this.sourceRect.clone(); }
-		this._cloneProps(bmp);
-		return bmp;
-	}
 
 }
